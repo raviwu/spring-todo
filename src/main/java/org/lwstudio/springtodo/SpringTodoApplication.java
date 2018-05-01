39b1ee018ed7ec3a53f9fa37135cf92ed2486543
Ravi Wu <raviwu@gmail.com>
2018-05-01 15:39:03 +0800

    Add home route and update dependencies

--- src/main/java/org/lwstudio/springtodo/SpringTodoApplication.java
+++ src/main/java/org/lwstudio/springtodo/SpringTodoApplication.java
@@ -2,9 +2,19 @@ package org.lwstudio.springtodo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class SpringTodoApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringTodoApplication.class, args);
