9bb8fbddc45843e376ddaf8c64008923bd55ba01
Ravi Wu <raviwu@gmail.com>
2018-05-01 15:23:30 +0800

    Initial Repo

---
+++ src/main/java/org/lwstudio/springtodo/SpringTodoApplication.java
@@ -0,0 +1,12 @@
+package org.lwstudio.springtodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTodoApplication.class, args);
+	}
+}
